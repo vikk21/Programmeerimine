75a1c6befc4bff157460af9df49b1848f31fab18
vikk21 <99194533+vikk21@users.noreply.github.com>
2023-02-15 09:48:30 +0200

    Update sammud.py

--- sammud.py
+++ sammud.py
@@ -1,14 +1,14 @@
 sammude_list=[]
-f = open('sammud.txt')#teeb .txt faili lahti
+f = open('sammud.txt')#Teeb .txt faili lahti
 for rida in f:
-    sammude_list.append(rida.strip())#loob iga failis olevale reale listis indeksi koha
-f.close()#sulgeb faili
-print(sammude_list) #prindib sammude listi
-#listi int failitüübiks tegemine
+    sammude_list.append(rida.strip())#Loob iga failis olevale reale listis indeksi koha
+f.close()#Sulgeb faili
+print(sammude_list) #Prindib sammude listi
+#Int listi failitüübiks tegemine
 with open('sammud.txt') as f:
     sammude_list = [ int(i) for i in f ]
-sammud_kokku = sum(sammude_list) #liidab sammude listi
-print("Sammude arv kokku:" , sammud_kokku)
-print("Keskmine sammude kogus:" , sum(sammude_list)/len(sammude_list))#keskmine arvutamine
-print("Väikseim sammude kogus:" , min(sammude_list)) #vähima arvutamine
-print("Suurim sammude kogus:" , max(sammude_list)) #suurima arvutamine
+sammud_kokku = sum(sammude_list) #Liidab sammude listi
+print("Sammude arv kokku:" , sammud_kokku) #Sammud kokku
+print("Keskmine sammude kogus:" , sum(sammude_list)/len(sammude_list))#Keskmine arvutamine
+print("Väikseim sammude kogus:" , min(sammude_list)) #Vähima arvutamine
+print("Suurim sammude kogus:" , max(sammude_list)) #Suurima arvutamine
